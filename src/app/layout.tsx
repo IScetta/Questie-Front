b05b9e4c579afaa7330e7dd1587cd56a9a0e3f43
IvanRojas FT47b <exequieliv@outlook.es>
2024-05-08 19:47:10 -0300

    columnCurse

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import ColumnCurso from "./components/columnCurso";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="es">
       <body className="flex flex-col min-h-screen justify-between">
         <Navbar />
+        <ColumnCurso />
         {children}
         <Footer />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      
     </div>
   );
 }
