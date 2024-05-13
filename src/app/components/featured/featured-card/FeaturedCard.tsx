d8147bad69839567636d99b492729e97124fd740
IScetta <150315667+IScetta@users.noreply.github.com>
2024-05-13 10:56:09 -0300

    act cards

--- src/app/components/featured/featured-card/FeaturedCard.tsx
+++ src/app/components/featured/featured-card/FeaturedCard.tsx
@@ -9,7 +9,7 @@ const FeaturedCard = ({course}:any) => {
   // console.log(course.id)
   return (
     <Link href={`/course-review/${course.id}`}>
-      <div className=" mx-2 w-[250px] h-[250px] ml-20 mr20 lg:w-40 duration-200 transform ease-in-out hover:scale-110 ">
+      <div className=" mx-2 w-[350px] h-[350px] ml-20 mr20 lg:w-40 duration-200 transform ease-in-out hover:scale-110 ">
         <div className=" my-11">
           <Image className="w-[250] h-[130]"
             src={course.image}
@@ -17,12 +17,12 @@ const FeaturedCard = ({course}:any) => {
             width={250}
             height={130}
           />
-          <div className="flex flex-col w-full h-[120px] bg-purpleMain text-white">
+          <div className="flex flex-col w-full h-[150px] bg-purpleMain text-white">
             <h2 className="mx-2 text-xl">{course.title}</h2>
-            <h4 className="bg-yellowMain text-purpleMain inline-flex items-baseline text-start px-2">
+            
+          </div><h4 className="flex flex-row bottom-div self-end bg-yellowMain text-purpleMain  items-baseline text-start px-2">
               {curso.infoGenral.categories}
             </h4>
-          </div>
         </div>
       </div>
     </Link>
