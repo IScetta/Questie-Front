a971ccf777bd4a3fe0c7d2b6c251f9ddf50d1796
LucasHeredia17 <heredialucas1@gmail.com>
2024-05-14 13:52:47 -0300

    fix: token in .env

--- src/app/create-lesson/page.tsx
+++ src/app/create-lesson/page.tsx
@@ -1,26 +1,19 @@
+// import ColumnCreateCourse from "../components/create-lesson/column-create-lesson/ColumnCreateLesson";
+// import ButtonCreator from "../components/create-lesson/button-creator/ButtonCreator";
+// import LessonTextComponent from "../components/create-lesson/lesson-components/lesson-text-component";
 
+// // import CreateLeesonText from "../components/create-lesson/create-lesson-text/CreateLessonText";
 
-import ColumnCreateCourse from "../components/create-lesson/Column-create-lesson/ColumnCreateLesson";
-import ButtonCreator from "../components/create-lesson/button-creator/ButtonCreator";
-import DraftEditor from "../components/create-lesson/create-lesson-text/CreateLessonText";
-import LessonTextComponent from "../components/create-lesson/lesson-components/lesson-text-component";
+// const CreateCourse = () => {
+//   return (
+//     <div className="flex flex-row mx-[11.5rem]">
+//       <div className="flex flex-grow-0">
+//         <ColumnCreateCourse />
+//       </div>
 
-// import CreateLeesonText from "../components/create-lesson/create-lesson-text/CreateLessonText";
+//       <ButtonCreator component={<LessonTextComponent />} />
+//     </div>
+//   );
+// };
 
-
-
-const CreateCourse = ()=>{
-    return(
-
-        <div className="flex flex-row mx-[11.5rem]">
-        <div className="flex flex-grow-0">
-            <ColumnCreateCourse/>
-        </div >
-            
-            <ButtonCreator component={<LessonTextComponent/>}/>
-            
-        </div>
-    )
-}
-
-export default CreateCourse;
+// export default CreateCourse;
--- src/helpers/lesson.helper.ts
+++ src/helpers/lesson.helper.ts
@@ -2,12 +2,13 @@ import { ILesson } from "@/app/types";
 import axios, { AxiosResponse } from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const TOKEN = process.env.NEXT_PUBLIC_TOKEN;
 
 const getLessons = async (): Promise<ILesson[]> => {
   try {
     const res: AxiosResponse<ILesson[]> = await axios.get(`${API_URL}lessons`, {
       headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc5MmZmNjUxLTc1NWYtNGNmYi1iYjVjLTRlYTJkYzJiM2I4NSIsImVtYWlsIjoiam9obkRvZUBnbWFpbC5jb20iLCJpc0FkbWluIjoiYWRtaW4iLCJzdWIiOiI3OTJmZjY1MS03NTVmLTRjZmItYmI1Yy00ZWEyZGMyYjNiODUiLCJpYXQiOjE3MTU2OTc0NTcsImV4cCI6MTcxNTcwNDY1N30.ZlxK0V5fwyEjIH5srFyutw_MPI6UraOzva5Ftvvtj6A`,
+        Authorization: `Bearer ${TOKEN}`,
       },
     });
     if (res.status !== 200) {
@@ -19,22 +20,20 @@ const getLessons = async (): Promise<ILesson[]> => {
   }
 };
 
-const getLessonById = async (
-  id: string
-): Promise<ILesson> => {
+const getLessonById = async (id: string): Promise<ILesson> => {
   try {
     const res: AxiosResponse<ILesson> = await axios.get(
       `${API_URL}lessons/${id}`,
       {
         headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc5MmZmNjUxLTc1NWYtNGNmYi1iYjVjLTRlYTJkYzJiM2I4NSIsImVtYWlsIjoiam9obkRvZUBnbWFpbC5jb20iLCJpc0FkbWluIjoiYWRtaW4iLCJzdWIiOiI3OTJmZjY1MS03NTVmLTRjZmItYmI1Yy00ZWEyZGMyYjNiODUiLCJpYXQiOjE3MTU2OTc0NTcsImV4cCI6MTcxNTcwNDY1N30.ZlxK0V5fwyEjIH5srFyutw_MPI6UraOzva5Ftvvtj6A`,
+          Authorization: `Bearer ${TOKEN}`,
         },
       }
     );
     if (res.status !== 200) {
       console.log(`Error status: ${res.status}`);
     }
-    
+
     return res.data;
   } catch (error) {
     throw new Error("Error al obtener el módulo");
