3866aeeeef917482b3c722427b0e493749e6d161
Joel Blanco <joeld.blanco@gmail.com>
2024-05-14 18:43:30 -0500

    chore: Update Categories page with Slider component and improve layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Login from "./components/auth/login";
 import Carousel from "./components/carousel";
 import CategoriesCards from "./components/categories/categories-cards/CategoriesCards";
 import FeaturedCarousel from "./components/featured/featured-carousel";
+import Slider from "./components/slider";
 
  const Home: React.FC = async ()=> {
   const courses = await getCoursesDB();
@@ -16,7 +17,12 @@ import FeaturedCarousel from "./components/featured/featured-carousel";
         <Login />
       </div>
       <div className="flex flex-col items-center justify-center w-full h-auto mt-16">
-        <FeaturedCarousel courses={courses}/>
+      <Slider
+          data={courses}
+          cardButtonLink="/course-review"
+          cardButtonLabel="View Course"
+          elementsPerSlide={3}
+        />
       </div>
       <div className="flex flex-col items-center justify-center w-full h-auto mt-10 mb-10 px-8 py-8 bg-blue-gray-50">
         <div className="w-full mb-6">
