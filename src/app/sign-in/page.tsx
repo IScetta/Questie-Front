01839c5c9bcf067673ca48f19695460c7ad176c4
LucasHeredia17 <heredialucas1@gmail.com>
2024-05-14 22:57:27 -0300

    update: login component fixed, pages sign-in/up added and register component added

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import Login from "../components/auth/login";
+
+const SignInPage = () => {
+  return (
+    <div className="flex items-center justify-center my-6">
+      <Login />
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import Register from "../components/auth/register";
+
+const SingUpPage = () => {
+  return (
+    <div className="flex items-center justify-center my-6">
+      <Register />
+    </div>
+  );
+};
+
+export default SingUpPage;
