13f81e8fac0a10aa1304d8bc863142a0beea0e1d
IScetta <150315667+IScetta@users.noreply.github.com>
2024-05-17 12:04:51 -0300

    act page create-course

--- src/app/components/create-course/create-course-column/CreateCourseColumn.tsx
+++ src/app/components/create-course/create-course-column/CreateCourseColumn.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
-import { FaCaretUp } from "react-icons/fa";
 
-const ColumnAdmin = async () => {
-    return (
-      <div className="justify-center items-center grid grid-cols-1">
+
+const CreateCourseColumn = ()=>{
+    return(
+        <div className="justify-center items-center grid grid-cols-1">
         <div className="h-full w-80 bg-purpleMainLight col-start-2 col-span-4 px-7 ">
           <div className="bg-image rounded-full h-52 w-52 ml-7 mt-6 mb-6"></div>
           <button className="bg-yellowMain text-purpleMain h-10 w-52 ml-7 text-lg mt-5">
@@ -22,7 +22,8 @@ const ColumnAdmin = async () => {
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default ColumnAdmin;
+    )
+}
+
+
+export default CreateCourseColumn;
