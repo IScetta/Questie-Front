a5efdcc6249c607da45c7c8584b25e2bce5df180
Joel Blanco <joeld.blanco@gmail.com>
2024-05-16 21:50:10 -0500

    refactor: Fix function name and remove commented code in invoices page

--- src/app/invoices/[id]/page.tsx
+++ src/app/invoices/[id]/page.tsx
@@ -1,14 +1,15 @@
 import axios from "axios";
 
-const showInvoice = async (id: string) => {
-  try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}invoices/${id}`
-    );
-    return response.data;
-  } catch (error: any) {
-    throw new Error(error.message);
-  }
+const ShowInvoice = async (id: string) => {
+  return <h1>Factura</h1>;
+  // try {
+  //   const response = await axios.get(
+  //     `${process.env.NEXT_PUBLIC_API_URL}invoices/${id}`
+  //   );
+  //   return response.data;
+  // } catch (error: any) {
+  //   throw new Error(error.message);
+  // }
 };
 
-export default showInvoice;
+export default ShowInvoice;
