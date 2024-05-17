a7274ad32c40ff8959cdc6909ab451a4578b12cd
IvanRojas FT47b <exequieliv@outlook.es>
2024-05-17 14:34:22 -0300

    el carrusel responsive

--- src/app/components/card/Card.tsx
+++ src/app/components/card/Card.tsx
@@ -35,7 +35,7 @@ const Card = ({
           <h5 className="text-xl font-bold tracking-tight text-gray-900 truncate">
             {title}
           </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">{body}</p>
+          <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-5">{body}</p>
         </div>
         <div className="w-full">
           <div className="w-full">
