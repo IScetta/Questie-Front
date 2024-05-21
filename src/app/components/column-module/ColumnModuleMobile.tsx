92de684585900c9780375942aabc6a6d2709d27c
LabM <bcdld@hotmail.com>
2024-05-21 14:15:57 -0600

    fix/minor_style_changes

--- src/app/components/column-module/ColumnModuleMobile.tsx
+++ src/app/components/column-module/ColumnModuleMobile.tsx
@@ -14,7 +14,7 @@ const ColumnModuleMobile = ({course}: {course: ICourse}) => {
         <div className={` justify-center items-center grid grid-cols-1 ${styles['ColumnModuleContainerMobile']}`}>
             <div className={`h-full w-80 bg-purpleMainLight col-start-2 col-span-4 px-7 ${styles['ColumnModuleMobile']} ${IsOpen ? styles['MyVisible'] : styles['MyHidden']}`}>
                 <Link href={`/course-review/${course.id}`}>
-                <div className="bg-whiteColumn my-5 py-3 mt-10 flex flex-nowrap w-full h-[4.5rem] content-center hover:bg-purpleMainLighter">
+                <div className={`bg-whiteColumn my-5 py-3 mt-10 flex flex-nowrap w-full h-[4.5rem] content-center hover:bg-purpleMainLighter ${styles['CourseTitle']}`}>
                     <div className="bg-image w-12 h-12 rounded-full ml-2"></div>
                     <div className="flex flex-col ml-2 justify-center w-[12.5rem]">
                     <p className="font-bold text-base line-clamp-1">{course.title}</p>
@@ -28,7 +28,7 @@ const ColumnModuleMobile = ({course}: {course: ICourse}) => {
                 {course.modules.length > 0 ? (
                 course.modules.map((module: any, index: any) => (
                     <Link href={`/module/${module.id}`} key={index}>
-                    <div className="bg-whiteColumn mb-4 hover:bg-purpleMainLighter pl-5 pr-5 py-3 w-full h-[4.5rem] content-center cursor-pointer">
+                    <div className={`bg-whiteColumn mb-4 hover:bg-purpleMainLighter pl-5 pr-5 py-3 w-full h-[4.5rem] content-center cursor-pointer ${styles['ModuleButton']}`}>
                         <p className="text-xs font-light">Módulo {numberModule++}</p>
                         <p className="text-base font-semibold line-clamp-1">
                         {module.title}
