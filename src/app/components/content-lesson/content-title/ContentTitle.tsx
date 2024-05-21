005ffc591b65003bfa043c3d941515dc82a7531c
LucasHeredia17 <heredialucas1@gmail.com>
2024-05-21 20:21:44 -0300

    update: render content finished

--- src/app/components/content-lesson/content-title/ContentTitle.tsx
+++ src/app/components/content-lesson/content-title/ContentTitle.tsx
@@ -1,11 +1,11 @@
-const ContentTitle: React.FC<{ title: string }> = ({
+const ContentTitle: React.FC<{ title: string | undefined }> = ({
   title,
 }: {
-  title: string;
+  title: string | undefined;
 }): JSX.Element => {
   return (
     <div className="bg-blue-gray-50 w-full h-auto p-4 mt-8 text-center rounded">
-      <h1 className="text-5xl font-semibold">{title}</h1>
+      <h1 className="text-4xl font-semibold">{title}</h1>
     </div>
   );
 };
--- src/app/components/content-lesson/content-video/ContentVideo.tsx
+++ src/app/components/content-lesson/content-video/ContentVideo.tsx
@@ -1,7 +1,12 @@
-const ContentVideo: React.FC<{ video: string }> = ({
+const ContentVideo: React.FC<{
+  video: string | undefined;
+  description: string | undefined;
+}> = ({
   video,
+  description,
 }: {
-  video: string;
+  video: string | undefined;
+  description: string | undefined;
 }): JSX.Element => {
   return (
     <div className="bg-blue-gray-50 flex justify-center items-center w-full h-auto p-4 mt-4">
@@ -9,7 +14,7 @@ const ContentVideo: React.FC<{ video: string }> = ({
         width="640px"
         height="480px"
         src={video}
-        title="YouTube video player"
+        title={description}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
