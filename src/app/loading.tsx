9629a791e83d1003a8a3e3cb1d5bea8f8fddd067
Joel Blanco <joeld.blanco@gmail.com>
2024-05-25 14:01:42 -0500

    [fix/loading]: Translated loading state's message from "Loading..." to "Cargando..."

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
   return (
     <div className="flex flex-col flex-wrap items-center justify-center gap-2 absolute w-screen h-screen bg-white top-0 left-0 opacity-95">
       <Spinner aria-label="Loading" size="xl" color="purple" />
-      Loading...
+      Cargando...
     </div>
   );
 };
