a1d3e76622f0d816fe927be5f79c6b8d354bc8fc
LucasHeredia17 <heredialucas1@gmail.com>
2024-05-29 16:33:33 -0300

    fixes

--- src/app/components/featured/featured-card/FeaturedCard.tsx
+++ src/app/components/featured/featured-card/FeaturedCard.tsx
@@ -1,17 +1,13 @@
-import { DatoNuevoCurso } from "@/helpers/categoriesPreLoad";
 import Image from "next/image";
 import Link from "next/link";
 
-
-
-const FeaturedCard = ({course}:any) => {
-  const curso = DatoNuevoCurso[0];
-  // console.log(course.id)
+const FeaturedCard = ({ course }: any) => {
   return (
     <Link href={`/course-review/${course.id}`}>
       <div className=" mx-2 w-[350px] h-[350px] ml-20 mr20 lg:w-40 duration-200 transform ease-in-out hover:scale-110 ">
         <div className=" my-11">
-          <Image className="w-[250] h-[130]"
+          <Image
+            className="w-[250] h-[130]"
             src={course.image}
             alt="js"
             width={250}
@@ -19,10 +15,7 @@ const FeaturedCard = ({course}:any) => {
           />
           <div className="flex flex-col w-full h-[150px] bg-purpleMain text-white">
             <h2 className="mx-2 text-xl">{course.title}</h2>
-            
-          </div><h4 className="flex flex-row bottom-div self-end bg-yellowMain text-purpleMain  items-baseline text-start px-2">
-              {curso.infoGenral.categories}
-            </h4>
+          </div>
         </div>
       </div>
     </Link>
